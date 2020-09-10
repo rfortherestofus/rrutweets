f89d461c48932281dfef60bd040ccc38d1f2d9f3
David Keyes <dgkeyes@gmail.com>
2020-09-10 16:25:04 -0700

    Get things set up for session with Thomas on automating tweeting

---
+++ R/automated_tweeting.R
@@ -0,0 +1,11 @@
+library(tidyverse)
+library(googlesheets4)
+
+rru_tweets_sheet <- "https://docs.google.com/spreadsheets/d/10ec07SNOSOmpzcSVgdcfm4Mg-3N_Y7gZVvv9oycM-og/edit#gid=91245060"
+
+rru_content_tweets <- read_sheet(rru_tweets_sheet,
+                                 sheet = "RRU Content Tweets")
+
+rru_content_tweets %>%
+  write_sheet(rru_tweets_sheet,
+              sheet = "RRU Content Tweets")
