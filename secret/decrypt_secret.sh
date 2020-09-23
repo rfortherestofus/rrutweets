2a95042bce01b38caead7d221d36aceddf2b9676
David Keyes <dgkeyes@gmail.com>
2020-09-23 11:47:32 -0700

    Fix .sh file

--- secret/decrypt_secret.sh
+++ secret/decrypt_secret.sh
@@ -1,2 +1,2 @@
-gpg --quiet --batch --yes --decrypt --passphrase="ENCYRPTION_PASSPHRASE" \
+gpg --quiet --batch --yes --decrypt --passphrase="$ENCYRPTION_PASSPHRASE" \
 --output ./secret/rrutweets-sheet.json ./secret/rrutweets-sheet.json.gpg
