5fbe9bd94eb36bfe515acf8acdf386eead140afc
David Keyes <dgkeyes@gmail.com>
2020-09-24 10:15:22 -0700

    Try promotional tweet again

--- inst/test-tweets.R
+++ inst/test-tweets.R
@@ -35,39 +35,39 @@ twitter_token <-
 
 # Get Tweets --------------------------------------------------------------
 
-rru_tweets_sheet <- Sys.getenv("SHEET_PATH")
-
-tweets <- read_sheet(rru_tweets_sheet,
-                     sheet = "Test Tweets") %>%
-  mutate(tweet_length = str_length(tweet_text)) %>%
-  filter(tweet_length < 280) %>%
-  filter(is.na(date_posted)) %>%
-  mutate(n = row_number()) %>%
-  mutate(date_posted = case_when(n == 1 ~ now()))
-
-tweet <- tweets %>%
-  filter(!is.na(date_posted)) %>%
-  pull(tweet_text)
-
-# Post Tweet --------------------------------------------------------------
-
-post_tweet(status = tweet,
-           token = twitter_token)
-
-# Put Tweets Back on Google Sheet -----------------------------------------
-
-get_last_tweet_url <- function() {
-
-  Sys.sleep(10)
-
-  rtweet::search_tweets(q = "rfortherest",
-                        include_rts = FALSE,
-                        token = twitter_token) %>%
-    dplyr::filter(screen_name == "rfortherest") %>%
-    dplyr::slice(1) %>%
-    dplyr::pull(status_url)
-}
-
-tweets %>%
-  write_sheet(rru_tweets_sheet,
-              sheet = "Test Tweets")
+# rru_tweets_sheet <- Sys.getenv("SHEET_PATH")
+#
+# tweets <- read_sheet(rru_tweets_sheet,
+#                      sheet = "Test Tweets") %>%
+#   mutate(tweet_length = str_length(tweet_text)) %>%
+#   filter(tweet_length < 280) %>%
+#   filter(is.na(date_posted)) %>%
+#   mutate(n = row_number()) %>%
+#   mutate(date_posted = case_when(n == 1 ~ now()))
+#
+# tweet <- tweets %>%
+#   filter(!is.na(date_posted)) %>%
+#   pull(tweet_text)
+#
+# # Post Tweet --------------------------------------------------------------
+#
+# post_tweet(status = tweet,
+#            token = twitter_token)
+#
+# # Put Tweets Back on Google Sheet -----------------------------------------
+#
+# get_last_tweet_url <- function() {
+#
+#   Sys.sleep(10)
+#
+#   rtweet::search_tweets(q = "rfortherest",
+#                         include_rts = FALSE,
+#                         token = twitter_token) %>%
+#     dplyr::filter(screen_name == "rfortherest") %>%
+#     dplyr::slice(1) %>%
+#     dplyr::pull(status_url)
+# }
+#
+# tweets %>%
+#   write_sheet(rru_tweets_sheet,
+#               sheet = "Test Tweets")
