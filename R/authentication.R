8619adf7fae235e7f2faa5c46a377975c6feb395
David Keyes <dgkeyes@gmail.com>
2020-10-09 16:13:34 -0700

    Testing

--- R/authentication.R
+++ R/authentication.R
@@ -6,14 +6,14 @@
 #' @examples
 authenticate_twitter <- function() {
 
-twitter_token <<-
-  rtweet::create_token(
-    app = "rrutweets",
-    consumer_key = Sys.getenv("TWITTER_API_KEY"),
-    consumer_secret = Sys.getenv("TWITTER_SECRET_KEY"),
-    access_token = Sys.getenv("TWITTER_ACCESS_TOKEN"),
-    access_secret = Sys.getenv("TWITTER_SECRET_TOKEN")
-  )
+  twitter_token <<-
+    rtweet::create_token(
+      app = "rrutweets",
+      consumer_key = Sys.getenv("TWITTER_API_KEY"),
+      consumer_secret = Sys.getenv("TWITTER_SECRET_KEY"),
+      access_token = Sys.getenv("TWITTER_ACCESS_TOKEN"),
+      access_secret = Sys.getenv("TWITTER_SECRET_TOKEN")
+    )
 
 }
 
@@ -25,18 +25,18 @@ twitter_token <<-
 #'
 #' @examples
 authenticate_googlesheets <- function() {
-if (rlang::is_interactive() == TRUE) {
+  if (rlang::is_interactive() == TRUE) {
 
-  googlesheets4::gs4_auth(
-    email = "david@rfortherestofus.com",
-    path = "/Users/davidkeyes/.R/gargle/r-for-the-rest-of-us-misc-c3cd7d281458.json",
-    scopes = "https://www.googleapis.com/auth/spreadsheets",
-    cache = gargle::gargle_oauth_cache(),
-    use_oob = gargle::gargle_oob_default(),
-    token = NULL
-  )
+    googlesheets4::gs4_auth(
+      email = "david@rfortherestofus.com",
+      path = "/Users/davidkeyes/.R/gargle/r-for-the-rest-of-us-misc-c3cd7d281458.json",
+      scopes = "https://www.googleapis.com/auth/spreadsheets",
+      cache = gargle::gargle_oauth_cache(),
+      use_oob = gargle::gargle_oob_default(),
+      token = NULL
+    )
 
-} else {
-  googlesheets4::gs4_auth(email = Sys.getenv("GOOGLE_MAIL"), path = "rrutweets-sheet.json")
-}
+  } else {
+    googlesheets4::gs4_auth(email = Sys.getenv("GOOGLE_MAIL"), path = "rrutweets-sheet.json")
+  }
 }
