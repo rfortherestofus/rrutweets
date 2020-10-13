87209a18718ebb9188772c495736536db73bc81a
David Keyes <dgkeyes@gmail.com>
2020-10-12 21:06:16 -0700

    Schedule get tweet data every day

--- inst/get_tweet_data.R
+++ inst/get_tweet_data.R
@@ -9,8 +9,6 @@ authenticate_twitter()
 authenticate_googlesheets()
 
 
-
-
 new_tweets <- search_tweets(q = "rfortherest",
                             include_rts = FALSE,
                             n = 1000,
@@ -21,16 +19,17 @@ new_tweets <- search_tweets(q = "rfortherest",
 
 
 
-old_tweets <- read_csv(system.file("twitter-export.csv",
-                                   package = "rrutweets")) %>%
-  clean_names() %>%
-  select(tweet_full_text,
-         tweet_favorite_count,
-         tweet_retweet_count) %>%
-  set_names("tweet", "likes", "retweets")
+# old_tweets <- read_csv(system.file("twitter-export.csv",
+#                                    package = "rrutweets")) %>%
+#   clean_names() %>%
+#   select(tweet_full_text,
+#          tweet_favorite_count,
+#          tweet_retweet_count) %>%
+#   set_names("tweet", "likes", "retweets")
 
 rru_tweets_sheet <- Sys.getenv("SHEET_PATH")
 
-bind_rows(new_tweets, old_tweets) %>%
+# bind_rows(new_tweets, old_tweets) %>%
+new_tweets %>%
   write_sheet(rru_tweets_sheet,
               sheet = "Tweet Performance")
