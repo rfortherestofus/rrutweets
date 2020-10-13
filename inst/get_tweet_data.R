a775f6b9180bb82eddd8c8a5b28b8035778460a4
David Keyes <dgkeyes@gmail.com>
2020-10-12 20:53:49 -0700

    Try get tweet data

---
+++ inst/get_tweet_data.R
@@ -0,0 +1,30 @@
+authenticate_twitter()
+authenticate_googlesheets()
+
+library(rtweet)
+library(tidyverse)
+library(janitor)
+library(lubridate)
+library(googlesheets4)
+
+
+new_tweets <- search_tweets(q = "rfortherest",
+                            include_rts = FALSE,
+                            n = 1000,
+                            retryonratelimit = TRUE) %>%
+  filter(screen_name == "rfortherest") %>%
+  select(text, favorite_count, retweet_count) %>%
+  set_names("tweet", "likes", "retweets")
+
+old_tweets <- read_csv("inst/twitter-export.csv") %>%
+  clean_names() %>%
+  select(tweet_full_text,
+         tweet_favorite_count,
+         tweet_retweet_count) %>%
+  set_names("tweet", "likes", "retweets")
+
+rru_tweets_sheet <- Sys.getenv("SHEET_PATH")
+
+bind_rows(new_tweets, old_tweets) %>%
+  write_sheet(rru_tweets_sheet,
+              sheet = "Tweet Performance")
