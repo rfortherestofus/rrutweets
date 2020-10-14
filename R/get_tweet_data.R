e6766e77e66c749a14de4a6f7ba11ac95b874103
David Keyes <dgkeyes@gmail.com>
2020-10-14 14:49:25 -0700

    Test

--- R/get_tweet_data.R
+++ R/get_tweet_data.R
@@ -1,30 +1,26 @@
-# authenticate_twitter()
-# authenticate_googlesheets()
-#
-# library(rtweet)
-# library(tidyverse)
-# library(janitor)
-# library(lubridate)
-# library(googlesheets4)
-#
-#
-# new_tweets <- search_tweets(q = "rfortherest",
-#               include_rts = FALSE,
-#               n = 1000,
-#               retryonratelimit = TRUE) %>%
-#   filter(screen_name == "rfortherest") %>%
-#   select(text, favorite_count, retweet_count) %>%
-#   set_names("tweet", "likes", "retweets")
-#
-# old_tweets <- read_csv("inst/twitter-export.csv") %>%
-#   clean_names() %>%
-#   select(tweet_full_text,
-#          tweet_favorite_count,
-#          tweet_retweet_count) %>%
-#   set_names("tweet", "likes", "retweets")
-#
-# rru_tweets_sheet <- Sys.getenv("SHEET_PATH")
-#
-# bind_rows(new_tweets, old_tweets) %>%
-#   write_sheet(rru_tweets_sheet,
-#               sheet = "Tweet Performance")
+#' Get tweet data and post to Google Sheet
+#'
+#' @return Posts data on Google Sheet
+#' @export
+#'
+#' @examples
+get_tweet_data <- function() {
+
+  authenticate_twitter()
+  authenticate_googlesheets()
+
+  tweets <- rtweet::search_tweets(q = "rfortherest",
+                                  include_rts = FALSE,
+                                  n = 1000,
+                                  retryonratelimit = TRUE) %>%
+    dplyr::filter(screen_name == "rfortherest") %>%
+    dplyr::select(text, favorite_count, retweet_count) %>%
+    purrr::set_names("tweet", "likes", "retweets") %>%
+    dplyr::arrange(desc(retweets))
+
+  rru_tweets_sheet <- Sys.getenv("SHEET_PATH")
+
+  tweets %>%
+    googlesheets4::write_sheet(rru_tweets_sheet,
+                               sheet = "Tweet Performance")
+}
